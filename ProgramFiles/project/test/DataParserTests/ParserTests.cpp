13473914524d0b989871e2c8820147f55e20412c
Maciej Salamonski <macieksalamonski@gmail.com>
2022-03-06 12:36:45 +0100

    tests refactor

--- ProgramFiles/project/test/DataParserTests/ParserTests.cpp
+++ ProgramFiles/project/test/DataParserTests/ParserTests.cpp
@@ -9,27 +9,35 @@
 namespace masalamo
 {
 
+const std::string PARSER_TEST_FILE_PATH = {"../test/constants/ParserTestFile.json"};
+const size_t EXPECTED_RESULT_CONTAINER_SIZE = 3;
+const std::vector<Team> EXPECTED_TEAM_CONTAINER
+{
+    {EGroup::EGroupA, 5, 0, 0, 5, 0, 3, 9, 1, 3, "WWW", "Uruguay"},
+    {EGroup::EGroupA, 4, 0, 4, 8, 1, 3, 6, 2, 2, "LWW", "Russia"},
+    {EGroup::EGroupH, -3, 0, 5, 2, 2, 3, 3, 4, 1, "LLL", "Poland"}
+};
+
+
 class ParserFixture : public ::testing::Test
 {
 protected:
-    const std::string PARSER_TEST_FILE_PATH = {"../test/constants/ParserTestFile.json"};
-    const size_t EXPECTED_RESULT_CONTAINER_SIZE = 3;
-    const std::vector<Team> EXPECTED_TEAM_CONTAINER
-    {
-        {EGroup::EGroupA, 5, 0, 0, 5, 0, 3, 9, 1, 3, "WWW", "Uruguay"},
-        {EGroup::EGroupA, 4, 0, 4, 8, 1, 3, 6, 2, 2, "LWW", "Russia"},
-        {EGroup::EGroupH, -3, 0, 5, 2, 2, 3, 3, 4, 1, "LLL", "Poland"}
-    };
 
-    std::unique_ptr<ApiCommunicatorMock> apiCommunicatorMockPtr_ = std::make_unique<ApiCommunicatorMock>();
-    std::unique_ptr<Reader> readerPtr_ = std::make_unique<Reader>(std::move(apiCommunicatorMockPtr_), PARSER_TEST_FILE_PATH);
+    void SetUp() override
+    {
+        apiCommunicatorMockPtr_ = std::make_unique<ApiCommunicatorMock>();
+        EXPECT_CALL(*apiCommunicatorMockPtr_, downloadData()).Times(1);
+
+        readerPtr_ = std::make_unique<Reader>(std::move(apiCommunicatorMockPtr_), PARSER_TEST_FILE_PATH);
+    }
+
+    std::unique_ptr<ApiCommunicatorMock> apiCommunicatorMockPtr_;
+    std::unique_ptr<Reader> readerPtr_;
 };
 
 
 TEST_F(ParserFixture, parserShouldReturnContainerWithProperFilledObjectsOfTypeTeam)
 {
-    //EXPECT_CALL(*apiCommunicatorMockPtr_, downloadData()).Times(1);
-
     std::unique_ptr<Parser> parserPtr = std::make_unique<Parser>(std::move(readerPtr_));
     const std::vector<Team> resultTeamsContainer = parserPtr->parse();
 
--- ProgramFiles/project/test/DataParserTests/ReaderTests.cpp
+++ ProgramFiles/project/test/DataParserTests/ReaderTests.cpp
@@ -10,35 +10,38 @@ namespace masalamo
 
 using TestPair = std::pair<nlohmann::json, std::string>;
 
+const std::string READER_TEST_FILE_PATH = {"../test/constants/TestFile.json"};
+const nlohmann::json EXPECTED_RESULT = {
+    {
+        {"name", "Molecule Man"},
+        {"age", 29},
+        {"secretIdentity", "Dan Jukes"},
+        {"powers", {
+            "Radiation resistance",
+            "Turning tiny",
+            "Radiation blast"
+        }},
+    },
+    {
+        {"name", "Madame Uppercut"},
+        {"age", 39},
+        {"secretIdentity", "Jane Wilson"},
+        {"powers", {
+            "Million tonne punch",
+            "Damage resistance",
+            "Superhuman reflexes"
+        }},
+    }
+};
+
+
 class ReaderFixture : public ::testing::Test
 {
 protected:
-    const std::string READER_TEST_FILE_PATH = {"../test/constants/TestFile.json"};
-    const nlohmann::json EXPECTED_RESULT = {
-        {
-            {"name", "Molecule Man"},
-            {"age", 29},
-            {"secretIdentity", "Dan Jukes"},
-            {"powers", {
-                "Radiation resistance",
-                "Turning tiny",
-                "Radiation blast"
-            }},
-        },
-        {
-            {"name", "Madame Uppercut"},
-            {"age", 39},
-            {"secretIdentity", "Jane Wilson"},
-            {"powers", {
-                "Million tonne punch",
-                "Damage resistance",
-                "Superhuman reflexes"
-            }},
-        }
-    };
 
     std::unique_ptr<ApiCommunicatorMock> apiCommunicatorMockPtr_ = std::make_unique<ApiCommunicatorMock>();
 };
+
 
 TEST_F(ReaderFixture, ShouldReturnJsonObject)
 {
